acaec9b9f213a67f11f86a2f8fbfd89192c0d65e
D3ad5h0t <alexgl007@mail.ru>
2024-09-05 14:53:36 +1000

    Added new service and K8S config files

---
+++ CommandsService/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+        
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound POST # Command Service");
+
+        return Ok("Inbound test of form Platforms Controller");
+    }
+}
